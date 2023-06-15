3bbfe445efa0b6e68b55cd58e82bc76cb7703a70
ggors <ggorski@consoneo.com>
2023-06-15 14:13:21 +0200

    🚧 Add animation for WhoAmI

--- front/src/pages/WhoAmI.tsx
+++ front/src/pages/WhoAmI.tsx
@@ -1,67 +1,68 @@
-    import styles from "../assets/css/WhoAmI.module.css";
-    import {motion, useScroll, useTransform} from "framer-motion";
-    import React, {useRef} from "react";
-    import {stylesWithCssVar} from "../utils/motion";
+import styles from "../assets/css/WhoAmI.module.css";
+import {motion, useScroll, useTransform} from "framer-motion";
+import React, {useRef} from "react";
+import {stylesWithCssVar} from "../utils/motion";
 
-    export default function whoAmI() {
-        const targetRef = useRef<HTMLDivElement | null>(null);
+export default function whoAmI() {
+    const targetRef = useRef<HTMLDivElement | null>(null);
 
-        const {scrollYProgress} = useScroll({
-            target: targetRef,
-            offset: ["start end", "end end"],
-        });
+    const {scrollYProgress} = useScroll({
+        target: targetRef,
+        offset: ["start end", "end end"],
+    });
 
-        const textX = useTransform(scrollYProgress, [0.5, 0.7], ["100%", "-100%"]);
-        const opacitySection = useTransform(scrollYProgress, [0.1, 0.5], [0, 1]);
-        const scale = useTransform(scrollYProgress, [0.1, 0.7], [1, 0.7]);
+    const textX = useTransform(scrollYProgress, [0.5, 0.7], ["100%", "-100%"]);
+    const opacitySection = useTransform(scrollYProgress, [0.1, 0.5], [0, 1]);
+    const scale = useTransform(scrollYProgress, [0.1, 0.7], [1, 0.7]);
 
-        const opacityBorder = useTransform(
-            scrollYProgress,
-            [0.7, 0.71, 0.72],
-            [1, 1, 0]
-        );
+    const opacityBorder = useTransform(
+        scrollYProgress,
+        [0.7, 0.71, 0.72],
+        [1, 1, 0]
+    );
 
-        const cssVars = {
-            "--scale": scale,
-            "--opacity-border": opacityBorder,
-        } as React.CSSProperties;
+    const cssVars = {
+        "--scale": scale,
+        "--opacity-border": opacityBorder,
+    } as React.CSSProperties;
 
-        const finalTextOpacity = useTransform(
-            scrollYProgress,
-            [0.7, 0.71, 0.72, 0.8, 0.9],
-            [0, 0, 1, 1, 0]
-        );
+    const finalTextOpacity = useTransform(
+        scrollYProgress,
+        [0.7, 0.71, 0.72, 0.8, 0.9],
+        [0, 0, 1, 1, 0]
+    );
 
-        const finalTextScale = useTransform(scrollYProgress, [0.8, 0.9], [1, 0.7]);
+    const finalTextScale = useTransform(scrollYProgress, [0.8, 0.9], [1, 0.7]);
 
-        console.log(scale)
-        console.log(opacityBorder)
-        return (
-            <section
-                // style={{
-                //     opacity: opacitySection,
-                // }}
-                ref={targetRef}
-                className={styles.whoAmI_section}
-            >
-                <div className={styles.whoAmI_container} >
-                    <motion.p aria-hidden className={styles.firsText} style={{x: textX, y: "-50%"}}>
-                        Qui suis-je ?
-                    </motion.p>
-                    <motion.p aria-hidden className={styles.secondText} style={{x: textX, y: "-50%"}}>
-                        Qui suis-je ?
-                    </motion.p>
-                    <motion.p className={styles.thirdText} style={{
-                        opacity: finalTextOpacity,
-                        scale: finalTextScale,
-                        y: "-50%",
-                        x: "-50%",
-                    }}>
-                        Découvrez <br/>
-                        le
-                    </motion.p>
-
-                </div>
-            </section>
-        )
-    }
+    console.log(scale)
+    console.log(opacityBorder)
+    return (
+        <section
+            // style={{
+            //     opacity: opacitySection,
+            // }}
+            ref={targetRef}
+            className={styles.whoAmI_section}
+        >
+            <div className={styles.whoAmI_container}>
+                <motion.p aria-hidden className={styles.firsText} style={{x: textX, y: "-50%"}}>
+                    Qui suis-je ?
+                </motion.p>
+                <motion.p aria-hidden className={styles.secondText} style={{x: textX, y: "-50%"}}>
+                    Qui suis-je ?
+                </motion.p>
+                <motion.p className={styles.thirdText} style={{
+                    opacity: finalTextOpacity,
+                    scale: finalTextScale,
+                    y: "-50%",
+                    x: "-50%",
+                }}>
+                    Découvrez <br/>
+                    le
+                </motion.p>
+                <span></span>
+                <span></span>
+            </div>
+        </section>
+    )
+}

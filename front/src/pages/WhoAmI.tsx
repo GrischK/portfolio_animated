52bbbc3fd094582820069bcdbd94e2817b13e9b4
ggors <ggorski@consoneo.com>
2023-06-16 18:17:30 +0200

    🐛 Fix behavior of <p> which inherit the section opacity

--- front/src/pages/WhoAmI.tsx
+++ front/src/pages/WhoAmI.tsx
@@ -1,7 +1,6 @@
 import styles from "../assets/css/WhoAmI.module.css";
-import {motion, useScroll, useTransform} from "framer-motion";
-import React, {useRef} from "react";
-import {stylesWithCssVar} from "../utils/motion";
+import {motion, useScroll, useSpring, useTransform} from "framer-motion";
+import {useRef} from "react";
 
 export default function whoAmI() {
     const targetRef = useRef<HTMLDivElement | null>(null);
@@ -11,58 +10,52 @@ export default function whoAmI() {
         offset: ["start end", "end end"],
     });
 
-    const textX = useTransform(scrollYProgress, [0.5, 0.7], ["100%", "-100%"]);
-    const opacitySection = useTransform(scrollYProgress, [0.1, 0.5], [0, 1]);
-    const scale = useTransform(scrollYProgress, [0.1, 0.7], [1, 0.7]);
+    const textX = useTransform(scrollYProgress, [0.5, 0.7], ["100%", "-200%"]);
+    const opacitySection = useTransform(scrollYProgress, [0.35, 0.5, 0.61, 0.65], [0, 1, 1, 0]);
+    const scale = useTransform(scrollYProgress, [0.35, 0.8], [1, 0.6]);
 
-    const opacityBorder = useTransform(
-        scrollYProgress,
-        [0.7, 0.71, 0.72],
-        [1, 1, 0]
+    const opacityBorder = useSpring(
+        useTransform(scrollYProgress, [0.7, 0.71, 0.72], [1, 1, 0])
     );
-
-    const cssVars = {
-        "--scale": scale,
-        "--opacity-border": opacityBorder,
-    } as React.CSSProperties;
 
     const finalTextOpacity = useTransform(
         scrollYProgress,
-        [0.7, 0.71, 0.72, 0.8, 0.9],
+        [0.60, 0.61, 0.62, 0.99, 1],
         [0, 0, 1, 1, 0]
     );
 
     const finalTextScale = useTransform(scrollYProgress, [0.8, 0.9], [1, 0.7]);
 
-    console.log(scale)
+    console.log(scrollYProgress)
     console.log(opacityBorder)
     return (
-        <section
-            // style={{
-            //     opacity: opacitySection,
-            // }}
+        <motion.section
+            style={{scale: scale}}
             ref={targetRef}
             className={styles.whoAmI_section}
         >
-            <div className={styles.whoAmI_container}>
+            <motion.div
+                style={{opacity: opacitySection}}
+                className={styles.whoAmI_container}>
                 <motion.p aria-hidden className={styles.firsText} style={{x: textX, y: "-50%"}}>
                     Qui suis-je ?
                 </motion.p>
                 <motion.p aria-hidden className={styles.secondText} style={{x: textX, y: "-50%"}}>
                     Qui suis-je ?
                 </motion.p>
-                <motion.p className={styles.thirdText} style={{
-                    opacity: finalTextOpacity,
-                    scale: finalTextScale,
-                    y: "-50%",
-                    x: "-50%",
-                }}>
-                    Découvrez <br/>
-                    le
-                </motion.p>
+
                 <span></span>
                 <span></span>
-            </div>
-        </section>
+            </motion.div>
+            <motion.p className={styles.thirdText} style={{
+                opacity: finalTextOpacity,
+                scale: finalTextScale,
+                y: "-50%",
+                x: "-50%",
+            }}>
+                Découvrez <br/>
+                le
+            </motion.p>
+        </motion.section>
     )
 }
